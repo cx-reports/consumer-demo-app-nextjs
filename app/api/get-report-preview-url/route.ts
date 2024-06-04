b06de6405cd27e0c58116de71e3b26583eadecd8
Stefan <stefan.dragicevic@codaxy.com>
2024-06-04 08:52:38 +0200

    Add push temoporary data

--- app/api/get-report-preview-url/route.ts
+++ app/api/get-report-preview-url/route.ts
@@ -1,20 +1,25 @@
 import { CxReportsClient } from "@cx-reports/api-client";
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  let client = new CxReportsClient({
-    baseUrl: process.env.BASE_URL!,
-    authToken: process.env.AUTH_TOKEN!,
-    defaultWorkspaceId: process.env.DEFAULT_WORKSPACE_ID,
-  });
+let client = new CxReportsClient({
+  baseUrl: process.env.BASE_URL!,
+  authToken: process.env.AUTH_TOKEN!,
+  defaultWorkspaceId: process.env.DEFAULT_WORKSPACE_ID,
+});
 
+export async function GET(req: NextRequest, res: NextResponse) {
   try {
     let { nonce } = await client.createNonceAuthToken();
 
+    const url = new URL(req.url);
+    const searchParams = new URLSearchParams(url.searchParams);
+    const tmpDataId = searchParams.get("tempDataId");
+
+    console.log(url, tmpDataId);
     let previewUrl = client.getReportPreviewURL({
       reportId: 18620,
       nonce,
+      tmpDataId: tmpDataId !== null ? parseInt(tmpDataId) : undefined,
     });
 
     return NextResponse.json({ previewUrl });
--- components/modules/preview-sheet.tsx
+++ components/modules/preview-sheet.tsx
@@ -1,24 +1,44 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import { SheetContent, SheetHeader } from "@/components/ui/sheet";
 import { useEffect, useState } from "react";
 
 export function PreviewSheet() {
   const [previewUrl, setPreviewUrl] = useState();
+  // const [tempDataId, setTempDataId] = useState();
 
-  const getPreviewUrl = async () => {
-    let { previewUrl } = await fetch("/api/get-report-preview-url", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res?.json());
+  const getPreviewUrl = async (tempDataId: number) => {
+    let { previewUrl } = await fetch(
+      `/api/get-report-preview-url?tempDataId=${tempDataId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => res?.json());
 
     setPreviewUrl(previewUrl);
   };
 
+  const pushTemporaryData = async () => {
+    let temporaryDataId = await fetch("/api/push-temporary-data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content:
+          "{\n  \u0022invoice_number\u0022: \u00222023-ABC-123456\u0022,\n  \u0022date\u0022: \u00222023-11-03\u0022,\n  \u0022due_date\u0022: \u00222023-11-17\u0022,\n  \u0022bill_to\u0022: {\n    \u0022company_name\u0022: \u0022Tech Solutions Inc.\u0022,\n    \u0022address\u0022: \u0022123 Tech Street\u0022,\n    \u0022city\u0022: \u0022Innovate City\u0022,\n    \u0022state\u0022: \u0022Techsylvania\u0022,\n    \u0022zip\u0022: \u002212345\u0022,\n    \u0022country\u0022: \u0022USA\u0022\n  },\n  \u0022ship_to\u0022: {\n    \u0022company_name\u0022: \u0022Tech Solutions Inc.\u0022,\n    \u0022address\u0022: \u0022456 Solution Ave\u0022,\n    \u0022city\u0022: \u0022Progress Town\u0022,\n    \u0022state\u0022: \u0022Techsylvania\u0022,\n    \u0022zip\u0022: \u002267890\u0022,\n    \u0022country\u0022: \u0022USA\u0022\n  },\n  \u0022items\u0022: [\n    {\n      \u0022item_id\u0022: \u0022CPU001\u0022,\n      \u0022description\u0022: \u0022Quad-core CPU 3.6 GHz\u0022,\n      \u0022unit_price\u0022: 250.00,\n      \u0022quantity\u0022: 2,\n      \u0022subtotal\u0022: 500.00\n    },\n    {\n      \u0022item_id\u0022: \u0022RAM002\u0022,\n      \u0022description\u0022: \u002216GB DDR4 RAM\u0022,\n      \u0022unit_price\u0022: 90.00,\n      \u0022quantity\u0022: 4,\n      \u0022subtotal\u0022: 360.00\n    },\n    {\n      \u0022item_id\u0022: \u0022HDD003\u0022,\n      \u0022description\u0022: \u00221TB SSD Hard Drive\u0022,\n      \u0022unit_price\u0022: 150.00,\n      \u0022quantity\u0022: 1,\n      \u0022subtotal\u0022: 150.00\n    },\n    {\n      \u0022item_id\u0022: \u0022GPU004\u0022,\n      \u0022description\u0022: \u0022Gaming Graphics Card\u0022,\n      \u0022unit_price\u0022: 400.00,\n      \u0022quantity\u0022: 1,\n      \u0022subtotal\u0022: 400.00\n    },\n    {\n      \u0022item_id\u0022: \u0022PSU005\u0022,\n      \u0022description\u0022: \u0022750W Power Supply Unit\u0022,\n      \u0022unit_price\u0022: 80.00,\n      \u0022quantity\u0022: 1,\n      \u0022subtotal\u0022: 80.00\n    },\n    {\n      \u0022item_id\u0022: \u0022CASE006\u0022,\n      \u0022description\u0022: \u0022Mid Tower Computer Case\u0022,\n      \u0022unit_price\u0022: 120.00,\n      \u0022quantity\u0022: 1,\n      \u0022subtotal\u0022: 120.00\n    }\n  ],\n  \u0022subtotals\u0022: {\n    \u0022items_total\u0022: 1610.00,\n    \u0022tax_rate\u0022: 0.07,\n    \u0022tax_amount\u0022: 112.70,\n    \u0022shipping_fee\u0022: 25.00,\n    \u0022discount\u0022: 0.00\n  },\n  \u0022total_due\u0022: 1747.70,\n  \u0022notes\u0022: \u0022Thank you for your business!\u0022\n}",
+      }),
+    }).then((res) => res?.json());
+
+    await getPreviewUrl(temporaryDataId.tempDataId);
+  };
+
   useEffect(() => {
-    getPreviewUrl();
+    pushTemporaryData();
   }, []);
 
   return (
@@ -29,7 +49,7 @@ export function PreviewSheet() {
           <Button variant="outline">PDF</Button>
         </div>
       </SheetHeader>
-      <div className="grid gap-4 py-4 h-full">
+      <div className="grid h-full gap-4 py-4">
         <iframe src={previewUrl} className="w-full h-full" />
       </div>
     </SheetContent>
